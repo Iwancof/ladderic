ded66d8fbecd1ab3d18bc6ae0529d2b7b48ca800
Iwancof <rock.iwancof@gmail.com>
2022-06-16 16:08:25 +0900

    Change activate function signiture

--- src/network.rs
+++ src/network.rs
@@ -1,7 +1,6 @@
 use alloc::vec::Vec;
 
-use core::cell::RefCell;
-use core::fmt::Display;
+use core::fmt::{Debug, Display};
 use core::ops::{Add, Mul};
 
 pub trait Network<InputScl, OutputScl>
@@ -11,12 +10,12 @@ where
 {
     fn output_size(&self) -> usize;
 
-    fn forward_in_place_unchecked(&self, inp: &[InputScl], out: &mut [OutputScl]);
-    fn forward_in_place(&self, inp: &[InputScl], out: &mut [OutputScl]) {
+    fn forward_in_place_unchecked(&mut self, inp: &[InputScl], out: &mut [OutputScl]);
+    fn forward_in_place(&mut self, inp: &[InputScl], out: &mut [OutputScl]) {
         assert_eq!(self.output_size(), out.len());
         self.forward_in_place_unchecked(inp, out);
     }
-    fn forward(&self, inp: &[InputScl]) -> Vec<OutputScl> {
+    fn forward(&mut self, inp: &[InputScl]) -> Vec<OutputScl> {
         let mut out = (0..self.output_size())
             .map(|_| OutputScl::default())
             .collect::<Vec<_>>();
@@ -33,10 +32,23 @@ pub struct Neuron<WeightScl, BiasScl> {
     bias: BiasScl,
 }
 
-#[derive(Debug)]
 pub struct Layer<ValueScl, WeightScl, BiasScl> {
     params: Vec<Neuron<WeightScl, BiasScl>>,
-    afp: fn(ValueScl) -> ValueScl,
+    afp: fn(&Vec<ValueScl>, &ValueScl) -> ValueScl,
+}
+
+impl<V, W, B> Debug for Layer<V, W, B>
+where
+    V: Debug,
+    W: Debug,
+    B: Debug,
+{
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        write!(f, "Layer {{ param: {:?}, ", self.params)?;
+        write!(f, "afp: {:p} }}", self.afp as *const ())?;
+
+        Ok(())
+    }
 }
 
 #[derive(Debug)]
@@ -45,7 +57,8 @@ where
     ValueScl: Mul<WeightScl, Output = ValueScl>,
     ValueScl: Add<BiasScl, Output = ValueScl>,
 {
-    working: [RefCell<Vec<ValueScl>>; 2],
+    // working: [RefCell<Vec<ValueScl>>; 2],
+    working: [Vec<ValueScl>; 2],
     input_size: usize,
     // pub layers: Vec<(Vec<(Vec<WeightScl>, BiasScl)>, fn(ValueScl) -> ValueScl)>,
     pub layers: Vec<Layer<ValueScl, WeightScl, BiasScl>>,
@@ -61,7 +74,7 @@ where
 {
     fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
         for Layer { params, afp } in &self.layers {
-            writeln!(f, "{:?}:", afp)?;
+            writeln!(f, "{:p}:", afp as *const _)?;
             for Neuron { weight, bias } in params {
                 write!(f, "  weight: ")?;
                 for b in weight {
@@ -86,7 +99,7 @@ where
 {
     pub fn new_with_gen<WeightGenerator, BiasGenerator>(
         input_size: usize,
-        count_act: &[(usize, fn(ValueScl) -> ValueScl)],
+        count_act: &[(usize, fn(&Vec<ValueScl>, &ValueScl) -> ValueScl)],
         gen_weight: WeightGenerator,
         gen_bias: BiasGenerator,
     ) -> Self
@@ -94,10 +107,17 @@ where
         WeightGenerator: Fn(usize, usize, usize) -> WeightScl,
         BiasGenerator: Fn(usize, usize) -> BiasScl,
     {
-        let max = count_act.iter().max().unwrap().0;
+        let mut max = usize::MIN;
+        for t in count_act {
+            if max <= t.0 {
+                max = t.0;
+            }
+        }
+        let max = max;
+
         let working = [
-            RefCell::new((0..max).map(|_| ValueScl::default()).collect()),
-            RefCell::new((0..max).map(|_| ValueScl::default()).collect()),
+            (0..max).map(|_| ValueScl::default()).collect(),
+            (0..max).map(|_| ValueScl::default()).collect(),
         ];
         let layers = count_act
             .iter()
@@ -130,7 +150,10 @@ where
         }
     }
 
-    pub fn new(input_size: usize, count_act: &[(usize, fn(ValueScl) -> ValueScl)]) -> Self {
+    pub fn new(
+        input_size: usize,
+        count_act: &[(usize, fn(&Vec<ValueScl>, &ValueScl) -> ValueScl)],
+    ) -> Self {
         DenseNetwork::new_with_gen(
             input_size,
             count_act,
@@ -154,21 +177,29 @@ where
         self.layers.last().unwrap().params.len()
     }
 
-    fn forward_in_place_unchecked(&self, inp: &[ValueScl], out: &mut [ValueScl]) {
+    fn forward_in_place_unchecked(&mut self, inp: &[ValueScl], out: &mut [ValueScl]) {
         assert_eq!(inp.len(), self.input_size);
 
-        for (buf, inp) in self.working[0].borrow_mut().iter_mut().zip(inp.iter()) {
+        for (buf, inp) in self.working[0].iter_mut().zip(inp.iter()) {
             *buf = inp.clone();
             // Use buffer[0] as input.
         }
 
         let mut last_write = 0;
         for (i, Layer { params, afp }) in self.layers.iter().enumerate() {
-            let from = self.working[i % 2].borrow_mut(); // Previous layer.
-            let mut to = self.working[(i + 1) % 2].borrow_mut(); // Current layer.
-            last_write = (i + 1) % 2;
+            // let from = &self.working[i % 2]; // Previous layer.
+            // let to = &mut self.working[(i + 1) % 2]; // Current layer.
 
-            // TODO: BUG! `to` must be restricted accessing by currett layer neurons number.
+            let [ref mut zero, ref mut one] = self.working;
+
+            let (from, to) = if i % 2 == 0 {
+                last_write = 1;
+                (zero, one)
+            } else {
+                last_write = 0;
+                (one, zero)
+            };
+
             for (to, Neuron { weight, bias }) in to.iter_mut().zip(params.iter()) {
                 // to: Destination. wei: Weight. bia: Bias
                 *to = ValueScl::default();
@@ -177,13 +208,14 @@ where
                 for (from, wei) in from.iter().zip(weight.iter()) {
                     *to = to.clone() + (from.clone() * wei.clone());
                 }
+
                 *to = to.clone() + bias.clone();
 
-                *to = afp(to.clone());
+                *to = afp(from, to);
             }
         }
 
-        for (buf, out) in self.working[last_write].borrow_mut().iter_mut().zip(out) {
+        for (buf, out) in self.working[last_write].iter_mut().zip(out) {
             *out = buf.clone();
         }
     }
@@ -194,8 +226,8 @@ mod tests {
     use super::*;
     use alloc::vec;
 
-    fn linear(x: f64) -> f64 {
-        x
+    fn linear(_: &Vec<f64>, x: &f64) -> f64 {
+        *x
     }
 
     #[test]
@@ -233,7 +265,7 @@ mod tests {
     fn random_network() {
         use rand::Rng;
 
-        let dn = DenseNetwork::<f64, f64, f64>::new_with_gen(
+        let mut dn = DenseNetwork::<f64, f64, f64>::new_with_gen(
             3,
             &[
                 (5, linear),
